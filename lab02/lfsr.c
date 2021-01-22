a8c78dc931e1428d100c82f585e41ed4cdcb8929
han xinhang <noname98@126.com>
2021-01-22 22:21:02 +0800

    finish ex

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -3,8 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lfsr.h"
+#include "bit_ops.h"
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+
+   uint16_t input=(*reg)^(*reg>>2)^(*reg>>3)^(*reg>>5);
+
+   *reg=(*reg>>1)|(input<<15);
+
+
+
 }
 
